2496c53e33f1cfd10875d3b89a0aa2966c71764b
elenacorrea (From Hummingbird) <elena.correa@noaa.gov>
2020-04-08 13:45:52 -0700

    something

---
+++ run-array-job.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/bash
+
+#SBATCH --time=02:00:00
+#SBATCH --output=run-array-out-%A_%a
+#SBATCH --error=run-array-error-%A_%a
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user=elena.correa@noaa.gov
+#SBATCH --array=1-9
+
+
+./map-N-files-from-K.sh 128 $((SLURM_ARRAY_TASK_ID * 128 + 1))
